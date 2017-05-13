d177a10497c28bc2eb66228103435481b619ead2
BlackStar1991 <zirkaandry@ukr.net>
2017-05-13 12:37:00 +0300

    first commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://carabin.zyxel.in.ua/admin/');
+define('HTTP_CATALOG', 'http://carabin.zyxel.in.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://carabin.zyxel.in.ua/admin/');
+define('HTTPS_CATALOG', 'http://carabin.zyxel.in.ua/');
+
+// DIR
+define('DIR_APPLICATION', '/home/zyxel149/domains/zyxel.in.ua/public_html/carabin/admin/');
+define('DIR_SYSTEM', '/home/zyxel149/domains/zyxel.in.ua/public_html/carabin/system/');
+define('DIR_IMAGE', '/home/zyxel149/domains/zyxel.in.ua/public_html/carabin/image/');
+define('DIR_LANGUAGE', '/home/zyxel149/domains/zyxel.in.ua/public_html/carabin/admin/language/');
+define('DIR_TEMPLATE', '/home/zyxel149/domains/zyxel.in.ua/public_html/carabin/admin/view/template/');
+define('DIR_CONFIG', '/home/zyxel149/domains/zyxel.in.ua/public_html/carabin/system/config/');
+define('DIR_CACHE', '/home/zyxel149/domains/zyxel.in.ua/public_html/carabin/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/zyxel149/domains/zyxel.in.ua/public_html/carabin/system/storage/download/');
+define('DIR_LOGS', '/home/zyxel149/domains/zyxel.in.ua/public_html/carabin/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/zyxel149/domains/zyxel.in.ua/public_html/carabin/system/storage/modification/');
+define('DIR_UPLOAD', '/home/zyxel149/domains/zyxel.in.ua/public_html/carabin/system/storage/upload/');
+define('DIR_CATALOG', '/home/zyxel149/domains/zyxel.in.ua/public_html/carabin/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'zyxel149_carabin');
+define('DB_PASSWORD', 'carabin');
+define('DB_DATABASE', 'zyxel149_carabin');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://carabin.zyxel.in.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://carabin.zyxel.in.ua/');
+
+// DIR
+define('DIR_APPLICATION', '/home/zyxel149/domains/zyxel.in.ua/public_html/carabin/catalog/');
+define('DIR_SYSTEM', '/home/zyxel149/domains/zyxel.in.ua/public_html/carabin/system/');
+define('DIR_IMAGE', '/home/zyxel149/domains/zyxel.in.ua/public_html/carabin/image/');
+define('DIR_LANGUAGE', '/home/zyxel149/domains/zyxel.in.ua/public_html/carabin/catalog/language/');
+define('DIR_TEMPLATE', '/home/zyxel149/domains/zyxel.in.ua/public_html/carabin/catalog/view/theme/');
+define('DIR_CONFIG', '/home/zyxel149/domains/zyxel.in.ua/public_html/carabin/system/config/');
+define('DIR_CACHE', '/home/zyxel149/domains/zyxel.in.ua/public_html/carabin/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/zyxel149/domains/zyxel.in.ua/public_html/carabin/system/storage/download/');
+define('DIR_LOGS', '/home/zyxel149/domains/zyxel.in.ua/public_html/carabin/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/zyxel149/domains/zyxel.in.ua/public_html/carabin/system/storage/modification/');
+define('DIR_UPLOAD', '/home/zyxel149/domains/zyxel.in.ua/public_html/carabin/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'zyxel149_carabin');
+define('DB_PASSWORD', 'carabin');
+define('DB_DATABASE', 'zyxel149_carabin');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
